2f2e04f7d92f7e4ffb4e4d08eb4913677f33d57c
Bartosz Elert <bartoszelert@Bartoszs-MacBook-Pro.local>
2023-01-27 19:44:18 +0100

    mobile adjustments

--- src/components/app-layout/app-layout.tsx
+++ src/components/app-layout/app-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen w-full max-w-screen-xl p-8 mx-auto flex flex-col">
+    <div className="min-h-screen w-full max-w-screen-xl p-8 mx-auto flex flex-col overflow-hidden">
       <Outlet />
     </div>
   );
