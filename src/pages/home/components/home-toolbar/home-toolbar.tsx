80fb1862eeeac1f35c7442fbd6903d055d6919f9
Bartosz Elert <bartoszelert@Bartoszs-MacBook-Pro.local>
2023-01-27 20:19:35 +0100

    mobile adjustments

--- src/pages/home/components/home-toolbar/home-toolbar.tsx
+++ src/pages/home/components/home-toolbar/home-toolbar.tsx
@@ -2,16 +2,23 @@ import { ChangeEvent } from 'react';
 
 type HomeToolbarProps = {
   count: number;
+  isFetching: boolean;
   handleSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 export default function HomeToolbar({
   count,
+  isFetching,
   handleSearchChange,
 }: HomeToolbarProps) {
   return (
     <header className="flex flex-col gap-2">
-      <h1 className="text-xl text-gray-100 font-medium">Results ({count})</h1>
-      <div>
+      <h1 className="text-xl text-gray-100 font-medium">
+        Results ({count}){' '}
+        <span className="text-xs tracking-wide font-light text-blue-300">
+          {isFetching ? 'Fetching results...' : ''}
+        </span>
+      </h1>
+      <div className="relative flex flex-col gap-2">
         <label htmlFor="name-filter" />
         <input
           id="name-filter"
